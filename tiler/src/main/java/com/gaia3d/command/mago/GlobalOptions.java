ac72bc493a8036f8263387150bf5da3cff3d901c
znkim <znkim@gaia3d.com>
2024-03-20 15:12:21 +0900

    conflict resolved

--- tiler/src/main/java/com/gaia3d/command/mago/GlobalOptions.java
+++ tiler/src/main/java/com/gaia3d/command/mago/GlobalOptions.java
@@ -29,11 +29,10 @@ public class GlobalOptions {
 
     private static final String DEFAULT_INPUT_FORMAT = "kml";
     private static final String DEFAULT_OUTPUT_FORMAT = "b3dm";
-    //private static final int DEFAULT_NODE_LIMIT = 1024;
+    private static final String DEFAULT_INSTANCE_FILE = "instance.dae";
     private static final int DEFAULT_MIN_LOD = 0;
     private static final int DEFAULT_MAX_LOD = 3;
-    private static final int DEFAULT_POINT_LIMIT = 65536;
-    private static final int DEFAULT_POINT_SCALE = 2;
+    private static final int DEFAULT_POINT_LIMIT = 262144;
     private static final byte DEFAULT_MULTI_THREAD_COUNT = 4;
     private static final String DEFAULT_CRS = "4326";
     private static final String DEFAULT_NAME_COLUMN = "name";
@@ -41,7 +40,7 @@ public class GlobalOptions {
     private static final String DEFAULT_ALTITUDE_COLUMN = "altitude";
     private static final double DEFAULT_ABSOLUTE_ALTITUDE = 0.0d;
     private static final double DEFAULT_MINIMUM_HEIGHT = 1.0d;
-    private static final double DEFAULT_SKIRT_HEIGHT = 10.0d;
+    private static final double DEFAULT_SKIRT_HEIGHT = 4.0d;
     private static final boolean DEFAULT_DEBUG_LOD = false;
 
 
@@ -60,7 +59,10 @@ public class GlobalOptions {
     private String inputPath; // input file or dir path
     private String outputPath; // output dir path
     private String logPath; // log file path
+
     private String terrainPath; // terrain file path
+    private String instancePath; // instance file path
+
     private String inputFormat; // input file format
     private String outputFormat; // output file format
 
@@ -68,8 +70,6 @@ public class GlobalOptions {
     private String proj; // default projection
 
     private int pointLimit; // point limit per tile
-    private int pointScale; // point scale
-
     private int nodeLimit; // node limit per tile
     private int minLod; // minimum level of detail
     private int maxLod; // maximum level of detail
@@ -131,6 +131,13 @@ public class GlobalOptions {
             instance.setTerrainPath(command.getOptionValue(ProcessOptions.TERRAIN.getArgName()));
             validateInputPath(new File(instance.getTerrainPath()).toPath());
         }
+
+        if (command.hasOption(ProcessOptions.INSTANCE_FILE.getArgName())) {
+            instance.setInstancePath(command.getOptionValue(ProcessOptions.INSTANCE_FILE.getArgName()));
+        } else {
+            instance.setInstancePath(instance.getInputPath() + File.separator + DEFAULT_INSTANCE_FILE);
+        }
+
         if (command.hasOption(ProcessOptions.PROJ4.getArgName())) {
             instance.setProj(command.hasOption(ProcessOptions.PROJ4.getArgName()) ? command.getOptionValue(ProcessOptions.PROJ4.getArgName()) : null);
             CoordinateReferenceSystem crs = null;
@@ -163,7 +170,6 @@ public class GlobalOptions {
 
         /* Point Cloud Options */
         instance.setPointLimit(command.hasOption(ProcessOptions.MAX_POINTS.getArgName()) ? Integer.parseInt(command.getOptionValue(ProcessOptions.MAX_POINTS.getArgName())) : DEFAULT_POINT_LIMIT);
-        instance.setPointScale(/*command.hasOption(ProcessOptions.POINT_SCALE.getArgName()) ? Integer.parseInt(command.getOptionValue(ProcessOptions.POINT_SCALE.getArgName())) : */DEFAULT_POINT_SCALE);
 
         /* 2D Data Column Options */
         instance.setNameColumn(command.hasOption(ProcessOptions.NAME_COLUMN.getArgName()) ? command.getOptionValue(ProcessOptions.NAME_COLUMN.getArgName()) : DEFAULT_NAME_COLUMN);
