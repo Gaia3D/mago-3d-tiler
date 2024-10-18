abc0eee36befd91115ddb28cab41f51e45e4b9db
sdson <sdson@gaia3d.com>
2024-10-18 18:15:37 +0900

    chore : commit update

--- src/main/java/com/gaia3d/ExtensionModuleFrame.java
+++ src/main/java/com/gaia3d/ExtensionModuleFrame.java
@@ -1,6 +1,7 @@
 package com.gaia3d;
 
 import com.gaia3d.basic.model.GaiaScene;
+import com.gaia3d.basic.model.GaiaTexture;
 
 import java.util.Map;
 
@@ -10,4 +11,6 @@ public interface ExtensionModuleFrame {
     boolean isSupported();
 
     GaiaScene executePhotorealistic(GaiaScene gaiaScene, Map<String, Object> options);
+
+    GaiaTexture getRenderScene(GaiaScene scene);
 }
