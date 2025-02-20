a3b07056b2aef7a10a1d4a21e92617b73bd6a20d
znkim <znkim@gaia3d.com>
2025-02-20 18:55:30 +0900

    fix : change realistic mesh MIN_TEXTURE_SIZE 512px -> 32px

--- tiler/src/main/java/com/gaia3d/command/mago/GlobalOptions.java
+++ tiler/src/main/java/com/gaia3d/command/mago/GlobalOptions.java
@@ -57,7 +57,7 @@ public class GlobalOptions {
 
     public static final int REALISTIC_LOD0_MAX_TEXTURE_SIZE = 1024;
     public static final int REALISTIC_MAX_TEXTURE_SIZE = 1024;
-    public static final int REALISTIC_MIN_TEXTURE_SIZE = 512;
+    public static final int REALISTIC_MIN_TEXTURE_SIZE = 32;
     public static final int REALISTIC_SCREEN_DEPTH_TEXTURE_SIZE = 256;
     public static final int REALISTIC_SCREEN_COLOR_TEXTURE_SIZE = 1024;
     public static final double REALISTIC_LEAF_TILE_SIZE = 25.0; // meters
