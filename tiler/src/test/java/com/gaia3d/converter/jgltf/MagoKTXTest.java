029495914e42e7041a8807405b4c069925c278e3
znkim <znkim@gaia3d.com>
2025-03-27 11:05:35 +0900

    chore : add KTX test code

---
+++ tiler/src/test/java/com/gaia3d/converter/jgltf/MagoKTXTest.java
@@ -0,0 +1,21 @@
+package com.gaia3d.converter.jgltf;
+
+import com.gaia3d.command.Configurator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class MagoKTXTest {
+
+    @Test
+    void loadPNGImageFromBufferedImage() {
+        Configurator.initConsoleLogger();
+
+        String input = "D:\\workspace\\input\\pizza.jpg";
+        String output = "D:\\workspace\\input\\pizza.ktx";
+
+        MagoKTX magoKTX = new MagoKTX();
+        magoKTX.start(input, output);
+
+    }
+}
