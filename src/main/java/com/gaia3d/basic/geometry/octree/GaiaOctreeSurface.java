9488adbe545a4932ad964e4692fdcbfb45185d95
sdson <sdson@gaia3d.com>
2025-08-07 12:19:08 +0900

    feat: GaiaOctree ABSTRACT

---
+++ src/main/java/com/gaia3d/basic/geometry/octree/GaiaOctreeSurface.java
@@ -0,0 +1,13 @@
+package com.gaia3d.basic.geometry.octree;
+
+public class GaiaOctreeSurface<E> extends GaiaOctree<E> {
+
+    public GaiaOctreeSurface(GaiaOctreeSurface<E> parent) {
+        super(parent);
+    }
+
+
+    public void distributeContent() {
+
+    }
+}
