0186e0a5c20c743affced07ed01c8c2a6807f959
znkim <znkim@gaia3d.com>
2026-06-08 09:49:04 +0900

    fix: prevent zero instance processing bug to Instanced3DModelTiler

--- mago-tiler/src/main/java/com/gaia3d/process/tileprocess/tile/Instanced3DModelTiler.java
+++ mago-tiler/src/main/java/com/gaia3d/process/tileprocess/tile/Instanced3DModelTiler.java
@@ -133,6 +133,9 @@ public class Instanced3DModelTiler extends DefaultTiler implements Tiler {
         long instanceLimit = globalOptions.getMaxInstance();
         long instanceCount = tileInfos.size();
         boolean isRefineAdd = globalOptions.isRefineAdd();
+        if (instanceCount <= 0) {
+            return;
+        }
 
         GaiaBoundingBox gaiaBoundingBox = parentNode.getBoundingBox();
         if (gaiaBoundingBox == null) {
