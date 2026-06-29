0ddf2f0e53ef57a5ba864bb4451b0fda163753bc
ssdkorean <ssdkorean@gmail.com>
2026-06-29 14:22:43 +0900

    chore: code iproving and cleaning

--- mago-common/src/main/java/com/gaia3d/basic/remesher/PlaneHEdgeIntersectionType.java
+++ mago-common/src/main/java/com/gaia3d/basic/remesher/PlaneHEdgeIntersectionType.java
@@ -1,6 +1,7 @@
 package com.gaia3d.basic.remesher;
 
 public enum PlaneHEdgeIntersectionType {
+    // intersection type between a plane and a half-edge
     NONE,
     START_VERTEX,
     END_VERTEX,
